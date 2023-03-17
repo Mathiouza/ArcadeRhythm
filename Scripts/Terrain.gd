93ddcad37f21dcd85f05a53b68936cb94e667452
mathiouza <mathiouza@live.fr>
2023-03-17 19:08:49 -0400

    Adds hover and gameover condition

--- Scripts/Terrain.gd
+++ Scripts/Terrain.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+signal gameover
+
 var Piece = preload("res://Scenes/Piece.tscn")
 var Square = preload("res://Scenes/Square.tscn")
 
-enum SHAPE {L3 = 0, L4 = 1, PLUS5 = 2, I2 = 3, I3 = 4, RL4 = 5, T = 6}
+enum SHAPE {L3 = 0, I2 = 1, I3 = 2, S2 = 3, C = 4}
 
 const shapes = [
 	[
@@ -13,19 +15,6 @@ const shapes = [
 	],
 	[
 		Vector2(0, 0),
-		Vector2(0, -1),
-		Vector2(0, -2),
-		Vector2(1, 0)
-	],
-	[
-		Vector2(0, 0),
-		Vector2(1, 0),
-		Vector2(0, 1),
-		Vector2(-1, 0),
-		Vector2(0, -1)
-	],
-	[
-		Vector2(0, 0),
 		Vector2(1, 0)
 	],
 	[
@@ -35,21 +24,19 @@ const shapes = [
 	],
 	[
 		Vector2(0, 0),
-		Vector2(0, -1),
-		Vector2(0, -2),
-		Vector2(-1, 0)
+		Vector2(1, 1),
 	],
 	[
 		Vector2(0, 0),
+		Vector2(1, 1),
 		Vector2(0, 1),
-		Vector2(1, 0),
-		Vector2(-1, 0)
+		Vector2(1, 0)
 	]
 ]
 
 export var controller = 0
-export var width = 12
-export var height = 12
+export var width = 6
+export var height = 6
 
 var current_piece : Piece
 var next_piece : Piece
@@ -60,46 +47,39 @@ var square_positions: PoolVector2Array = []
 func _ready():
 	randomize()
 	
-	next_piece = Piece.instance()
-	next_piece.square_positions = shapes[randi() % SHAPE.size()]
-	$NextPieceContainer.add_child(next_piece)
-	
-	create_piece()
-
-func create_piece():
-	current_piece = Piece.instance()
-	current_piece.x = 3
-	current_piece.y = 3
-	current_piece.square_positions = shapes[randi() % SHAPE.size()]
-	add_child(current_piece)
+	place()
 
 func _process(delta):
-	if Input.is_action_just_pressed(str(controller)+"up") && check_before_movement(current_piece.x, current_piece.y-1, current_piece.rot, current_piece.square_positions):
+	if Input.is_action_just_pressed(str(controller)+"up") && check_for_edges(current_piece.x, current_piece.y-1, current_piece.rot, current_piece.square_positions):
 		current_piece.move(0, -1)
 	
-	if Input.is_action_just_pressed(str(controller)+"right") && check_before_movement(current_piece.x+1, current_piece.y, current_piece.rot, current_piece.square_positions):
+	if Input.is_action_just_pressed(str(controller)+"right") && check_for_edges(current_piece.x+1, current_piece.y, current_piece.rot, current_piece.square_positions):
 		current_piece.move(1, 0)
 	
-	if Input.is_action_just_pressed(str(controller)+"down") && check_before_movement(current_piece.x, current_piece.y+1, current_piece.rot, current_piece.square_positions):
+	if Input.is_action_just_pressed(str(controller)+"down") && check_for_edges(current_piece.x, current_piece.y+1, current_piece.rot, current_piece.square_positions):
 		current_piece.move(0, 1)
 	
-	if Input.is_action_just_pressed(str(controller)+"left") && check_before_movement(current_piece.x-1, current_piece.y, current_piece.rot, current_piece.square_positions):
+	if Input.is_action_just_pressed(str(controller)+"left") && check_for_edges(current_piece.x-1, current_piece.y, current_piece.rot, current_piece.square_positions):
 		current_piece.move(-1, 0)
 	
-	if Input.is_action_just_pressed(str(controller)+"red1") && check_before_movement(current_piece.x, current_piece.y, current_piece.rot+1, current_piece.square_positions):
+	if Input.is_action_just_pressed(str(controller)+"red1") && check_for_edges(current_piece.x, current_piece.y, current_piece.rot+1, current_piece.square_positions):
 		current_piece.turn(true)
 	
-	if Input.is_action_just_pressed(str(controller)+"red2") && check_before_movement(current_piece.x, current_piece.y, current_piece.rot-1, current_piece.square_positions):
+	if Input.is_action_just_pressed(str(controller)+"red2") && check_for_edges(current_piece.x, current_piece.y, current_piece.rot-1, current_piece.square_positions):
 		current_piece.turn(false)
 	
-	if Input.is_action_just_pressed(str(controller)+"yellow1"):
+	if Input.is_action_just_pressed(str(controller)+"yellow1") && check_for_placement(current_piece.x, current_piece.y, current_piece.rot, current_piece.square_positions):
+		place()
+
+func place():
+	if current_piece != null:
 		var positions = current_piece.get_block_positions()
 		
 		for block_pos in positions:
 			square_positions.append(block_pos)
 			
 			var new_square = Square.instance()
-			new_square.color = current_piece.color
+			new_square.color = Color(0.5, 0.5, 0.5)
 			new_square.x = block_pos.x
 			new_square.y = block_pos.y
 			squares.append(new_square)
@@ -108,44 +88,46 @@ func _process(delta):
 		check_for_clears()
 		
 		current_piece.queue_free()
-		
-		var blocks = next_piece.square_positions
-		var next_color = next_piece.color
-		
+	
+	var blocks = shapes[randi() % SHAPE.size()]
+	
+	if next_piece != null:
+		blocks = next_piece.square_positions
 		next_piece.queue_free()
-		next_piece = Piece.instance()
-		next_piece.square_positions = shapes[randi() % SHAPE.size()]
-		next_piece.color = Color(randf(), randf(), randf())
-		$NextPieceContainer.add_child(next_piece)
-		
-		var possible_placements = []
-		
-		for i in range(width):
-			for j in range(height):
-				for k in range(4):
-					if check_before_movement(i, j, k, blocks):
-						possible_placements.append([i, j, k])
-		
-		if possible_placements.size() == 0:
-			print("gameover")
-		else:
-			var placement = possible_placements[randi() % possible_placements.size()]
-			
-			current_piece = Piece.instance()
-			
-			current_piece.x = placement[0]
-			current_piece.y = placement[1]
-			current_piece.rot = placement[2]
-			current_piece.square_positions = blocks
-			current_piece.color = next_color
-			
-			add_child(current_piece)
+	
+	next_piece = Piece.instance()
+	next_piece.square_positions = shapes[randi() % SHAPE.size()]
+	$NextPieceContainer.add_child(next_piece)
+	
+	current_piece = Piece.instance()
+	
+	current_piece.x = 2
+	current_piece.y = 2
+	current_piece.rot = 0
+	current_piece.square_positions = blocks
+	
+	add_child(current_piece)
 
-func check_before_movement(offsetX, offsetY, offsetRot, blocks) -> bool:
+func force_placement():
+	if check_for_placement(current_piece.x, current_piece.y, current_piece.rot, current_piece.square_positions):
+		emit_signal("gameover")
+	else:
+		place()
+
+func check_for_placement(offsetX, offsetY, offsetRot, blocks) -> bool:
 	var positions = get_block_positions_on_terrain(offsetX, offsetY, offsetRot, blocks)
 	
 	for block_pos in positions:
 		if block_pos in square_positions || block_pos.x < 0 || block_pos.y < 0 || block_pos.x >= width || block_pos.y >= height:
+			return false
+	
+	return true
+
+func check_for_edges(offsetX, offsetY, offsetRot, blocks) -> bool:
+	var positions = get_block_positions_on_terrain(offsetX, offsetY, offsetRot, blocks)
+	
+	for block_pos in positions:
+		if block_pos.x < 0 || block_pos.y < 0 || block_pos.x >= width || block_pos.y >= height:
 			return false
 	
 	return true
